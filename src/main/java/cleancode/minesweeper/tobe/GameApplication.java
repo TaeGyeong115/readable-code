a3dd4b02477f894a4f78ed09b2740e50a8b41f2c
Taylor <115taegyeong@gmail.com>
2024-10-01 21:20:09 +0900

    feat: section4 OCP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -3,7 +3,9 @@ package cleancode.minesweeper.tobe;
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new VeryBeginner();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 
