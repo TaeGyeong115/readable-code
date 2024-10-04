6a218c9bf39a3c0b4be552b809cfe8ae28322025
Taylor <115taegyeong@gmail.com>
2024-10-04 14:19:57 +0900

    feat: section6 버그 잡기

--- src/main/java/cleancode/minesweeper/tobe/mineswepper/board/cell/NumberCell.java
+++ src/main/java/cleancode/minesweeper/tobe/mineswepper/board/cell/NumberCell.java
@@ -32,7 +32,7 @@ public class NumberCell implements Cell {
 
     @Override
     public boolean isChecked() {
-        return cellState.isChecked();
+        return cellState.isOpened();
     }
 
     @Override
