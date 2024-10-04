b00a93aeb9504698168e72c260bffdba5927e5ed
Taylor <115taegyeong@gmail.com>
2024-10-04 14:10:40 +0900

    feat: section6 패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/mineswepper/io/InputHandler.java
@@ -0,0 +1,11 @@
+package cleancode.minesweeper.tobe.mineswepper.io;
+
+import cleancode.minesweeper.tobe.mineswepper.board.position.CellPosition;
+import cleancode.minesweeper.tobe.mineswepper.user.UserAction;
+
+public interface InputHandler {
+
+    UserAction getUserActionFromUser();
+
+    CellPosition getCellPositionFromUser();
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/mineswepper/io/sign/UncheckedCellSignProvider.java
@@ -0,0 +1,20 @@
+package cleancode.minesweeper.tobe.mineswepper.io.sign;
+
+import cleancode.minesweeper.tobe.mineswepper.board.cell.CellSnapshot;
+import cleancode.minesweeper.tobe.mineswepper.board.cell.CellSnapshotStatus;
+
+public class UncheckedCellSignProvider implements CellSignProvidable {
+
+    private static final String UNCHECKED_SIGN = "□";
+
+    @Override
+    public boolean supports(CellSnapshot cellSnapshot) {
+        return cellSnapshot.isSameStatue(CellSnapshotStatus.UNCHECKED);
+    }
+
+    @Override
+    public String provide(CellSnapshot cellSnapshot) {
+        return UNCHECKED_SIGN;
+    }
+
+}
